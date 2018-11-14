abbcfb6603f04bb909003a6f9d2fb414b7b911da
sb10207 <sb10207@naver.com>
2018-11-14 22:06:50 +0900

    Initial commit

---
+++ src/main/java/com/sbeen/segi/SegiApplication.java
@@ -0,0 +1,12 @@
+package com.sbeen.segi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SegiApplication.class, args);
+	}
+}
