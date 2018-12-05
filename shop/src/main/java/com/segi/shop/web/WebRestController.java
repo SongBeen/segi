57d5d04c76767839cb14ed90f2cb38836c0ebc2b
sb10207 <sb10207@naver.com>
2018-12-05 23:58:59 +0900

    spring-boot study chapter1,2

--- shop/src/main/java/com/segi/shop/web/WebRestController.java
+++ shop/src/main/java/com/segi/shop/web/WebRestController.java
@@ -1,14 +1,27 @@
 package com.segi.shop.web;
 
 
+import com.segi.shop.domain.posts.PostsRepository;
+import com.segi.shop.dto.posts.PostsSaveRequestDto;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class WebRestController {
+
+    private PostsRepository postsRepository;
 
     @GetMapping("/hello")
     public String hello() {
         return "HelloWorld";
+    }
+
+    @PostMapping("/posts")
+    public void savePosts(@RequestBody PostsSaveRequestDto dto){
+        postsRepository.save(dto.toEntity());
     }
 }
